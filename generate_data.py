2bd6e7e04284fdab2f4fa94f4de3055a865ce894
Ari Brill <aryeh.brill@gmail.com>
2025-12-26 08:51:46 -0500

    Generate and save data files

---
+++ generate_data.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy import sparse
+
+from percolation_dataset import PercolationDataset, GroundTruthFeatures
+
+n_datasets = 10
+dataset_size = 100000
+embedding_dimension = 128
+
+for seed in range(n_datasets):
+    print(f"Generating dataset {seed}...")
+    rng = np.random.default_rng(seed=seed)
+    dataset = PercolationDataset(rng=rng)
+    points, latents, X, y = dataset.construct_embed(size=dataset_size, d=embedding_dimension)
+    ground_truth_features = GroundTruthFeatures(points, latents)
+    gt_features = ground_truth_features.get_features()
+    np.savez_compressed(
+        f"percolation_dataset_size{dataset_size}_dim{embedding_dimension}_seed{seed}.npz",
+        X=X,
+        y=y,
+    )
+    sparse.save_npz(
+        f"percolation_dataset_size{dataset_size}_dim{embedding_dimension}_seed{seed}_gt_features.npz",
+        gt_features,
+    )
